1fbd4e6d291e8fa1904520f2aa7bbb72b7dbfb61
Michail Chebaev <make7650@rambler.ru>
2020-05-10 07:12:01 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/jun48gempkjcvsxs?svg=true)](https://ci.appveyor.com/project/Maik808/dz3-1seienite)
---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void submissionTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("David Vasin");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        }
+
+}
+
