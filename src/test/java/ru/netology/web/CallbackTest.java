3da00afa0d5e5fe577c5c476d289e90e268feb42
Mikhail Chebaev <make7650@rambler.ru>
2020-05-11 11:31:53 +0300

    Editing CallbackTest

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -9,18 +9,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 class CallbackTest {
     @Test
-    void submissionTest() {
+    void correctlyTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("David Vasin");
+        form.$("[data-test-id=name] input").setValue("Ваня Васин");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-        }
-    
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
     @Test
-    void validationTest() {
+    void correctlyTestV2() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ваня Васин");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void checkSurnameTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=name] input").setValue("David Vasin");
@@ -28,6 +39,17 @@ class CallbackTest {
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void checkPhoneNumberTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Ваня Васин");
+        form.$("[data-test-id=phone] input").setValue("879270000015");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=\"phone\"] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
 }
